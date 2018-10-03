4bafbbdee14ef5c10174ba526422ba3ff2b5154b
M4cs <maxlikescs@gmail.com>
2018-10-02 20:36:08 -0400

    Fix for Credits

--- core/contrib.py
+++ core/contrib.py
@@ -7,6 +7,6 @@ def credits():
     print "Contributor:           GitHub Link:             Contribution:"
     print "-" * 50
     print "Ryan0x1                @Ryan0x1                 Maintainer/Creator"
-    print "Macs                   M4cs                     Maintainer/Creator"
+    print "Macs                   @M4cs                    Maintainer/Creator"
     print "hargoniX               @hargoniX                Contributor"
     print "                                                - Exploits/Dupalgeddon2"
